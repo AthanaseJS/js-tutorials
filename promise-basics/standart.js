93f13b2eb5e4b266af484b238d69dad20dfa1af8
Athanase <amakaridi@gmail.com>
2021-12-26 21:11:49 +0400

    test

--- promise-basics/standart.js
+++ promise-basics/standart.js
@@ -2,7 +2,7 @@ console.log('Request data...')
 
 setTimeout(() => {
   console.log('pereparing data ...')
-
+  let num =1;
   const backEndData = {
     server: 'aws',
     port: 20000,
@@ -10,7 +10,7 @@ setTimeout(() => {
   }
 
   setTimeout(() => {
-    backEndData.modified = teue
+    backEndData.modified = true
     console.log('Data resive', bcakEndData)
   }, 2000)
 }, 2000)
