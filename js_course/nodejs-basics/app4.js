1dbebf629385b5ea87c328c8dd78f3b71112457c
Athanase <amakaridi@gmail.com>
2025-01-05 20:18:51 +0400

    update

--- js_course/nodejs-basics/app4.js
+++ js_course/nodejs-basics/app4.js
@@ -1,7 +1,7 @@
-const logger = require('./logger.js')
+import logger, { log, url } from "./logger.js";
 
-console.log(logger)
+console.log(logger);
 
-console.log('this is function ' + logger.log('message form app4.js '))
+console.log("this is function " + log("message form app4.js "));
 
-console.log('This is propertiers url ' + logger.url)
+console.log("This is properties url " + url);
--- js_course/nodejs-basics/logger.js
+++ js_course/nodejs-basics/logger.js
@@ -1,21 +1,22 @@
 //;(function (exports, require, __filename, __dirname) {
-console.log(__filename)
-console.log(__dirname)
+console.log(__filename);
+console.log(__dirname);
 
-var url = 'http://mylogger.io/log'
+var url = "http://mylogger.io/log";
 
 function log(message) {
   // Sent http request
 
-  console.log(message)
+  console.log(message);
 }
 
 // export (add) log function to module structure
-module.exports.log = log
+const _log = log;
+export { _log as log };
 
-// export (add) url vareable to module structure
-module.exports.url = url
+// export (add) url variable to module structure
+const _url = url;
+export { _url as url };
 
-// export (add) url varrablesa and named is endPoint
-module.exports.endPoint = url
+export const endPoint = url;
 //})
